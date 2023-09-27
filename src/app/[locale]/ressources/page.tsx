40d2971aaeb59e3807d5874a31d9241497e7ce9f
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-09-27 11:47:45 -0400

    fix build

--- src/app/[locale]/ressources/page.tsx
+++ src/app/[locale]/ressources/page.tsx
@@ -1,11 +1,9 @@
-'use client';
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Ressources',
   description: 'Ressources related to Balboa',
 }
-
 
 export default function Ressources() {
   <div></div>
