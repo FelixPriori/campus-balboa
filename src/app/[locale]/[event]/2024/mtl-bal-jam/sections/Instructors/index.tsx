2f1d7e98e09260e8f46ae1fd47a79e3abcc28591
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-10-20 12:51:19 -0400

    Add google calendar link

--- src/app/[locale]/[event]/2024/mtl-bal-jam/sections/Instructors/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/sections/Instructors/index.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from 'next-intl'
 import styles from './styles.module.scss'
 import LinesCircle from '@/assets/svgs/lines-circle'
 import HalfCircle from '@/assets/svgs/half-circle'
+import Image from 'next/image'
 
 export default function Instructors() {
     const t = useTranslations('Events.2024.MtlBalJam')
@@ -13,6 +14,10 @@ export default function Instructors() {
             </div>
             <div className={styles.text}>
                 <h2>{t('saveTheDate')}</h2>
+                <a className={styles.googleLink} target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=N21jNGU4cWRyNGJiMnFhdmxtMnIwbnRxcGQgaW5mb0BjYW1wdXNiYWxib2Eub3Jn&amp;tmsrc=info%40campusbalboa.org">
+                    <Image src="/google-calendar.png" width={32} height={32} alt="google calendar logo" />
+                    {t('googleCal')}
+                </a>
             </div>
             <div className={styles.linesCircle}>
                 <LinesCircle />
