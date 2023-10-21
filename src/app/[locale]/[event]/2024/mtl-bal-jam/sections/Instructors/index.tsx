6860a7304775202d9d1c8b98efa5efb94370528b
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-10-21 10:19:41 -0400

    add public link to gcal

--- src/app/[locale]/[event]/2024/mtl-bal-jam/sections/Instructors/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/sections/Instructors/index.tsx
@@ -22,10 +22,10 @@ export default function Instructors() {
             </div>
             <div className={styles.text}>
                 <h2>{t('saveTheDate')}</h2>
-                {/* <a className={`${styles.googleLink} ${roboto.className}`} target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=M2JjYzJhODlhMmk0MDVuM2c4MHFjZTdrMG0gaW5mb0BjYW1wdXNiYWxib2Eub3Jn&amp;tmsrc=info%40campusbalboa.org">
+                <a className={`${styles.googleLink} ${roboto.className}`} target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NHB2dmE2NWZpMGVicHQybnFmNzYxbGU3Y3QgY181NWIzOTE2YTcxNWIyYzg1MTBmYzY5MmQ1M2M1NWMzZDc4OWNjNDIzNDA5MzIxZGEyNjJmM2I5MzZmNzQyOGZkQGc&amp;tmsrc=c_55b3916a715b2c8510fc692d53c55c3d789cc423409321da262f3b936f7428fd%40group.calendar.google.com">
                     <Image src="/google-calendar.png" width={32} height={32} alt="google calendar logo" />
                     {t('googleCal')}
-                </a> */}
+                </a>
             </div>
             <div className={styles.linesCircle}>
                 <LinesCircle />
