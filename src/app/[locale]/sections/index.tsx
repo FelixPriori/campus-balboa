af10d68439c71ca5255ec469ed566bb8d6022bce
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-10-25 14:25:40 -0400

    add correct prefix

--- src/app/[locale]/sections/index.tsx
+++ src/app/[locale]/sections/index.tsx
@@ -1,7 +1,7 @@
 export {default as Hero} from "./Hero";
 export {default as MissionsSection} from "./Missions";
 export {default as AboutUsSection} from "./AboutUs";
-export {default as EventsSection} from './Events'
-export {default as FeaturedSection} from './Featured'
-export {default as Footer} from './Footer'
-export {default as Calendar} from './Calendar'
+export {default as EventsSection} from './Events';
+export {default as FeaturedSection} from './Featured';
+export {default as Footer} from './Footer';
+export {default as Calendar} from './Calendar';
