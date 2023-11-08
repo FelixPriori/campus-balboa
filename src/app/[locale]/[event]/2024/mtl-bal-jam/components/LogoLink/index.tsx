a84afd5b2273fb0b7f7101234a9947f1fe786557
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-11-08 16:23:08 -0500

    fix some translation issues

--- src/app/[locale]/[event]/2024/mtl-bal-jam/components/LogoLink/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/components/LogoLink/index.tsx
@@ -7,7 +7,7 @@ import { useLocale, useTranslations } from 'next-intl';
 
 export default function LogoLink() {
     const [href, setHref] = useState<string | null>(null)
-    const t = useTranslations('Events.2024.MtlBalJam');
+    const t = useTranslations('Events.2024.MtlBalJam.iconAlts');
     const locale = useLocale()
 
 
@@ -24,7 +24,7 @@ export default function LogoLink() {
 
     return href && (
         <Link className={styles.logoWrapper} href={href}>
-            <Image src="/mtl-bal-jam-logo-black.png" alt={t('logoAlt')} width={100} height={100} />
+            <Image src="/mtl-bal-jam-logo-black.png" alt={t('mbjLogo')} width={100} height={100} />
         </Link>
     )
 }
