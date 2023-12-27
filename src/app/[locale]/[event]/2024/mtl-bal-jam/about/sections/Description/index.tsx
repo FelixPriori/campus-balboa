80d1368c83a697e2ff773bb89fa147b7e2538643
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-12-27 10:22:55 -0500

    add logo background

--- src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
@@ -1,14 +1,18 @@
 "use client"
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
 import styles from './styles.module.scss'
 
 export default function Staff() {
-    const t = useTranslations('Events.2024.MtlBalJam.aboutPage.descriptionSection')
+    const t = useTranslations('Events.2024.MtlBalJam')
     return (
         <section className={styles.descriptionSection}>
-            <h2>{t('title')}</h2>
+            <h2>{t('aboutPage.descriptionSection.title')}</h2>
             <div className={styles.content}>
-                <p>{t.rich('description', { br: (chunks) => <br /> })}</p>
+                <div className={styles.logoContainer}>
+                    <Image src="/mtl-bal-jam-logo-white.png" height={150} width={150} alt={t('iconAlts.mbjLogo')} />
+                </div>
+                <p>{t.rich('aboutPage.descriptionSection.description', { br: (chunks) => <br /> })}</p>
             </div>
         </section>
     )
