8f61a39a4bb650bb6203b36ac9378ba06ddca776
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2023-12-27 10:37:31 -0500

    fix words, add link

--- src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
@@ -1,10 +1,13 @@
 "use client"
-import { useTranslations } from 'next-intl'
+import { useTranslations, useLocale } from 'next-intl'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from './styles.module.scss'
 
 export default function Staff() {
     const t = useTranslations('Events.2024.MtlBalJam')
+    const locale = useLocale();
+
     return (
         <section className={styles.descriptionSection}>
             <h2>{t('aboutPage.descriptionSection.title')}</h2>
@@ -12,7 +15,14 @@ export default function Staff() {
                 <div className={styles.logoContainer}>
                     <Image src="/mtl-bal-jam-logo-white.png" height={150} width={150} alt={t('iconAlts.mbjLogo')} />
                 </div>
-                <p>{t.rich('aboutPage.descriptionSection.description', { br: (chunks) => <br /> })}</p>
+                <p>
+                    {
+                        t.rich('aboutPage.descriptionSection.description', {
+                            br: () => <br />,
+                            campus: (chunks) => <Link className={styles.link} href={`/${locale}`}>{chunks}</Link>
+                        })
+                    }
+                </p>
             </div>
         </section>
     )
