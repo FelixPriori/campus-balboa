29671186281e235b243e8416be9f169400f4b84d
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-03-28 09:25:55 -0400

    add land acknowledgement

--- src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/Description/index.tsx
@@ -5,16 +5,14 @@ import Image from 'next/image'
 import styles from './styles.module.scss'
 
 export default function Staff() {
-    const t = useTranslations('Events.2024.MtlBalJam')
+    const t = useTranslations('Events.2024.MtlBalJam');
+    const tLand = useTranslations('LandAknowledgement');
     const locale = useLocale();
 
     return (
         <section className={styles.descriptionSection}>
             <h2>{t('aboutPage.descriptionSection.title')}</h2>
             <div className={styles.content}>
-                <div className={styles.logoContainer}>
-                    <Image src="/mtl-bal-jam-logo-white.png" height={150} width={150} alt={t('iconAlts.mbjLogo')} />
-                </div>
                 <p>
                     {
                         t.rich('aboutPage.descriptionSection.description', {
@@ -23,6 +21,15 @@ export default function Staff() {
                         })
                     }
                 </p>
+            </div>
+            <h2>{tLand('title')}</h2>
+            <div className={styles.content}>
+                <p>
+                    {tLand('text', { name: "MTL BAL JAM" })}
+                </p>
+            </div>
+            <div className={styles.logoContainer}>
+                <Image src="/mtl-bal-jam-logo-white.png" height={150} width={150} alt={t('iconAlts.mbjLogo')} />
             </div>
         </section>
     )
