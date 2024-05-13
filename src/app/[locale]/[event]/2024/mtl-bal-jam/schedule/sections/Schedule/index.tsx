e27f4e38c2efcaaf1e45c088a7e132259dd24b8a
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-05-13 14:22:53 -0400

    add link to venues

--- src/app/[locale]/[event]/2024/mtl-bal-jam/schedule/sections/Schedule/index.tsx
+++ src/app/[locale]/[event]/2024/mtl-bal-jam/schedule/sections/Schedule/index.tsx
@@ -2,6 +2,7 @@
 import { useTranslations } from 'next-intl'
 import styles from './styles.module.scss'
 import Day from '../../components/Day';
+import Link from 'next/link';
 
 type EventDays = 'friday' | 'saturday' | 'sunday'
 
@@ -13,6 +14,9 @@ export default function Schedule() {
     return (
         <section className={styles.scheduleSection}>
             <h2>{t('title')}</h2>
+            <div className={styles.linkWrapper}>
+                <Link className={styles.link} href={t('link.href')}>{t('link.text')}</Link>
+            </div>
             <div className={styles.scheduleWrapper}>
                 {days.map(d => <Day key={d} day={d} />)}
             </div>
