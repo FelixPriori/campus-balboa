6c2fc7734898a32a04dd1574268596da5008a036
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-09-03 11:54:40 -0400

    updates sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,174 +1,42 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://www.campusbalboa.org/fr',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/extracurriculaire-olga',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/extracurriculaire-olga',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/music',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/music',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/venue',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/venue',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/about',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/about',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/instructors',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/instructors',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/extra',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/extra',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/competitions',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/competitions',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/code-of-conduct',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/code-of-conduct',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/registration',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/registration',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/volunteering',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/volunteering',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/travel',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/travel',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/fr/evenements/2024/mtl-bal-jam/visiting',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://www.campusbalboa.org/en/events/2024/mtl-bal-jam/visiting',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    }
-  ]
+	return [
+		{
+			url: 'https://www.campusbalboa.org/fr',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 1,
+		},
+		{
+			url: 'https://www.campusbalboa.org/en',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 1,
+		},
+		{
+			url: 'https://www.campusbalboa.org/fr/evenements/2024/extracurriculaire-olga',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://www.campusbalboa.org/en/events/2024/extracurriculaire-olga',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://www.campusbalboa.org/fr/evenements/2024/campus-launch',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://www.campusbalboa.org/en/events/2024/campus-launch',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.8,
+		},
+	]
 }
