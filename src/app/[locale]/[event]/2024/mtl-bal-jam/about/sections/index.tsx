6fa45b122550936e033b5976954d1a2ed13bf6f6
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-09-03 11:52:42 -0400

    removes mtlbaljam related pages

--- src/app/[locale]/[event]/2024/mtl-bal-jam/about/sections/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Description } from './Description'
-export { default as Staff } from './Staff'
-export { default as Contact } from './Contact'
--- src/app/[locale]/[event]/2024/mtl-bal-jam/sections/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header'
-export { default as Instructors } from './Instructors'
-export { default as Venue } from './Venue'
-export { default as Music } from './Music'
-export { default as Footer } from './Footer'
-export { default as ComingSoon } from './ComingSoon'
