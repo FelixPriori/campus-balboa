f85f16a40139b86fffee13556679efb5d0ce470a
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-09-03 13:30:06 -0400

    deletes mtlbaljam related files and text, redirects to launch

--- src/app/[locale]/[event]/2024/page.tsx
+++ src/app/[locale]/[event]/2024/page.tsx
@@ -1,5 +1,5 @@
-import {redirect} from 'next/navigation';
+import { redirect } from 'next/navigation'
 
 export default function EventsPage() {
-    redirect(`2024/mtl-bal-jam`);
+	redirect(`2024/campus-launch`)
 }
--- src/app/[locale]/[event]/page.tsx
+++ src/app/[locale]/[event]/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-import {redirect} from 'next/navigation';
-import { useLocale } from 'next-intl';
+'use client'
+import { redirect } from 'next/navigation'
+import { useLocale } from 'next-intl'
 
 export default function EventsPage() {
-    const locale = useLocale()
-    if (locale === 'fr') {
-        redirect(`evenements/2024/mtl-bal-jam`);
-    } else {
-        redirect(`events/2024/mtl-bal-jam`);
-    }
+	const locale = useLocale()
+	if (locale === 'fr') {
+		redirect(`evenements/2024/campus-launch`)
+	} else {
+		redirect(`events/2024/campus-launch`)
+	}
 }
