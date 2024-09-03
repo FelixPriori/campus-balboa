8635ba2e9302931fd509bcd5e20f6047ac762ca9
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-09-03 11:52:06 -0400

    adds redirects

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
-const createNextIntlPlugin = require('next-intl/plugin');
+const createNextIntlPlugin = require('next-intl/plugin')
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    }
-};
+	images: {
+		unoptimized: true,
+	},
+	async redirects() {
+		return [
+			{
+				source: '/en/:event/2024/mtl-bal-jam/:slug*',
+				destination: 'https://mtlbaljam.org/en',
+				permanent: true,
+			},
+			{
+				source: '/fr/:event/2024/mtl-bal-jam/:slug*',
+				destination: 'https://mtlbaljam.org/fr',
+				permanent: true,
+			},
+		]
+	},
+}
 
-module.exports = withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig)
