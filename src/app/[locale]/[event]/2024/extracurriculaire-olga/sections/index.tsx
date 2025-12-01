9fe6c1274add7338ce361c3c5010196a2193c464
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2025-12-01 16:38:52 -0500

    removes static pages

--- src/app/[locale]/[event]/2024/extracurriculaire-olga/sections/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Hero } from './Hero'
-export { default as Prices } from './Prices'
-export { default as About } from './About'
-export { default as Schedule } from './Schedule'
-export { default as Footer } from './Footer'
