aef9c051da141e398c971a5ceed8a81be99e8357
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2025-12-01 16:49:19 -0500

    merge donate btn component with footer

--- src/app/_sections/Footer/index.tsx
+++ src/app/_sections/Footer/index.tsx
@@ -1,8 +1,27 @@
 import LandAcknowledgement from '../../_components/LandAcknowledgement'
 import styles from './styles.module.scss'
-import { FooterSection } from '@/app/_types/footer'
+import { DonateButtonType, FooterSection } from '@/app/_types/footer'
 import SocialMedia from '@/app/_components/SocialMedia'
-import DonateButton from '@/app/_components/PayPal/DonateButton'
+import Image from 'next/image'
+
+function DonateButton({ donateButton }: { donateButton: DonateButtonType }) {
+	return (
+		<a
+			className={styles.donateButton}
+			href={donateButton.href}
+			rel="noreferrer"
+			target="_blank"
+		>
+			<Image
+				src="/paypal-monogram-full-color.png"
+				width={18}
+				height={18}
+				alt={donateButton.iconAlt}
+			/>
+			{donateButton.text}
+		</a>
+	)
+}
 
 export default function Footer({
 	contact,
