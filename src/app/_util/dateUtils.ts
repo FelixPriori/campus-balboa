3365eb8ae41036fe7359d7cd68b5a661217996e5
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2026-03-05 08:41:27 -0500

    fixes timezon issue

--- src/app/_util/dateUtils.ts
+++ src/app/_util/dateUtils.ts
@@ -1,13 +1,19 @@
 import { Locale } from '@/i18n'
-import { format } from 'date-fns'
+import { format } from 'date-fns-tz'
 import { frCA, enCA } from 'date-fns/locale'
 
 export const formatDate = (date: string, locale: Locale) => {
 	const dateFnLocale = locale === 'fr' ? frCA : enCA
-	return format(new Date(date), 'd LLLL u', { locale: dateFnLocale })
+	return format(new Date(date), 'd LLLL u', {
+		locale: dateFnLocale,
+		timeZone: 'America/Toronto',
+	})
 }
 
 export const formatClassTime = (date: string, locale: Locale) => {
 	const dateFnLocale = locale === 'fr' ? frCA : enCA
-	return format(new Date(date), 'd LLLL u, p', { locale: dateFnLocale })
+	return format(new Date(date), 'd LLLL u, p', {
+		locale: dateFnLocale,
+		timeZone: 'America/Toronto',
+	})
 }
