2eaf8e78b2cfe2652dd6b3cf048dea8a97c89dfa
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2026-03-25 17:26:49 -0400

    adds content security policy frame ancestor

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const securityHeaders = [
   { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'Content-Security-Policy', value: "frame-ancestors 'self' https://app.contentful.com" },
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
   { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
